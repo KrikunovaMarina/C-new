78bd20d521f73f5ead7eda012ec143a46804b952
Marina <umarishka@bk.ru>
2022-08-21 13:08:29 +0300

    add number4

---
+++ N4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Number4
+int Max(int arg1, int arg2, int arg3)
+{
+  int result=arg1;
+  if (arg2> result) result = arg2;
+  if (arg3> result) result = arg3;
+  return result;
+}
+int a1 = 22;
+int b1 = 1113;
+int c1 = 930;
+int max = Max(a1, b1, c1);
+
+Console.WriteLine(max);
