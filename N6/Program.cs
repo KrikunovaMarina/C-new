6cc291bd529722f7f6a880722866024c3129bb65
Marina <umarishka@bk.ru>
2022-08-21 16:29:54 +0300

    add N6

---
+++ N6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+double numberA = int.Parse(Console.ReadLine());
+//Console.WriteLine(numberA/2);
+if (numberA % 2 == 1)
+{
+    Console.Write(numberA);
+Console.WriteLine(" - Нечетное");
+}
+else
+{
+    Console.Write(numberA);
+    Console.WriteLine(" - Четное");
+};
